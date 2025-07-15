52428f32fab314275975dd2330990e4f78c8ff5e
RadwanHegazy <radwangaber11@gmail.com>
2025-07-15 17:27:28 +0300

    init: user file app

---
+++ core/file/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from users.models import User
+
+class UserFile (models.Model) : 
+    name = models.CharField(max_length=225)
+    content_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        User,
+        related_name='userfile_owner',
+        on_delete=models.CASCADE,
+    )
+    
+    file_size_in_giga = models.FloatField(null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+    shared_with = models.ManyToManyField(
+        User,
+        related_name='userfile_shared_with',
+        blank=True
+    )
+
+    file = models.FileField(
+        upload_to='uploads',
+    )
